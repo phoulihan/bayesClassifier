5970e492d9ed1a9f5bc382491e311d7ab8e2db90
phoulihan <xilinx_altera@yahoo.com>
2016-07-15 08:41:37 -0400

    updated
    fixed label imbalance

--- sentiment.java
+++ sentiment.java
@@ -1,18 +1,24 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import java.util.TimeZone;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -21,14 +27,54 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 
-import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.bayes.NaiveBayesMultinomial;
-import weka.classifiers.meta.FilteredClassifier;
-import weka.classifiers.trees.J48;
-import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Add;
 import weka.filters.unsupervised.attribute.Remove;
 import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.datagenerators.DataGenerator;
+import weka.datagenerators.classifiers.classification.RDG1;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.AODE;
+import weka.classifiers.bayes.BayesNet;
+import weka.classifiers.bayes.BayesianLogisticRegression;
+import weka.classifiers.bayes.HNB;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.bayes.NaiveBayesMultinomial;
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.meta.AdaBoostM1;
+import weka.classifiers.meta.Bagging;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.trees.DecisionStump;
+import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.core.tokenizers.NGramTokenizer;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
 
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
@@ -40,31 +86,23 @@ public static void main(String[] args) throws Exception
 	{            	
 	    String theModel = "naiveBayesMulti"; //naiveBayesMulti, J48, naiveBayes 
 		double correctThresh = 70.0;
-		int theLimit = 1000;
-	    double theSlice = 0.90;
+		int theLimit = 2000;
+	    double theSlice = 0.40;
 	
 		Properties prop = new Properties();
 		prop.load(new FileInputStream("config.properties"));  
 		
-		String theDb = prop.getProperty("theDb");
-		String theCol = prop.getProperty("theCol");
-		String amznServer = prop.getProperty("theServer");
-		String port = prop.getProperty("port");
 		String otherDb = prop.getProperty("otherDb");
 		String otherCol = prop.getProperty("otherCol");
 		String otherServer = prop.getProperty("otherServer");
 		String otherPort = prop.getProperty("otherPort");
 
 	    String body = "";
-		String Neutral = "";
-		String Bearish = "";
-		String Bullish = "";
-		String Spam = "";
 		int ratings[] = new int[4];
 	    String[] theCols = null;
 		
-		FileWriter writerTrain = new FileWriter("myPATH/train.arff");
-		FileWriter writerTest = new FileWriter("myPATH/test.arff");
+		FileWriter writerTrain = new FileWriter("/home/ubuntu/code/theData/train.arff");
+		FileWriter writerTest = new FileWriter("/home/ubuntu/code/theData/test.arff");
 		
 		//NEUTRAL DATA//
 		DBObject getSentimentNeut = new BasicDBObject();
@@ -77,13 +115,17 @@ public static void main(String[] args) throws Exception
 		DBCollection collNeut = dbNeut.getCollection(otherCol);
 		
 		DBCursor cursorNeut = collNeut.find(getSentimentNeut).sort(new BasicDBObject("theDate",1)).limit(theLimit);
-		//System.out.println(cursorNeut.size());
+	//	System.out.println(cursorNeut.size());
 		String mongoData = "";
 		
 		cursorNeut.hasNext();
 		
 		Map<Integer, String> neutData = new HashMap<Integer, String>();
-	    int i = 0;
+		Map<Integer, String> bullData = new HashMap<Integer, String>();
+		Map<Integer, String> bearData = new HashMap<Integer, String>();
+	    int neutCnt = 0;
+	    int bullCnt = 0;
+	    int bearCnt = 0;
 	    while (cursorNeut.hasNext()) 
 	    {
 	    	JSONParser parser = new JSONParser();
@@ -106,7 +148,7 @@ public static void main(String[] args) throws Exception
 			ratings[3] = Integer.parseInt(ratTempSpam.toString());
 			
 			int maxIndex = 0;
-			for (int z = 0; z < ratings.length; z++)
+			for (int z = 1; z < ratings.length; z++)
 			{
 			   int newnumber = ratings[z];
 			   if ((newnumber > ratings[maxIndex]))
@@ -114,7 +156,7 @@ public static void main(String[] args) throws Exception
 				   maxIndex = z;
 			   }
 			}
-			if(maxIndex == 1 || maxIndex == 3)
+			//if(maxIndex == 1 || maxIndex == 3)
 			{
 				theCols = body.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?");
 					
@@ -144,150 +186,28 @@ public static void main(String[] args) throws Exception
 						temp = temp + " " + theOut[k]; 
 					}
 				} 
-				if(temp != "")
+				if(temp != "" && (maxIndex == 1 || maxIndex == 3))
 				{
-					neutData.put(i,temp);
-					i++;
+					neutData.put(neutCnt,temp);
+					neutCnt++;
+				}
+				if(temp != "" && maxIndex == 0)
+				{
+					bullData.put(bullCnt,temp);
+					bullCnt++;
+				}
+				if(temp != "" && maxIndex == 2)
+				{
+					bearData.put(bearCnt,temp);
+					bearCnt++;
 				}
 			}
 	    }
 	    cursorNeut.close();		
 		System.out.println(neutData.size());
-		
-		///BEAR DATA///
-		DBObject getSentimentBear = new BasicDBObject();
-		List<BasicDBObject> objBear = new ArrayList<BasicDBObject>();
-		objBear.add(new BasicDBObject("sentiment", "Bearish"));
-		getSentimentBear.put("$or", objBear);	
-	
-		Mongo m = new Mongo(amznServer, Integer.parseInt(port));
-		DB dbBear = m.getDB(theDb);
-		DBCollection collBear = dbBear.getCollection(theCol);
-		
-		DBCursor cursorBear = collBear.find(getSentimentBear).sort(new BasicDBObject("theDate",1)).limit(neutData.size()*2);
-		//System.out.println(cursorBear.size());
-		mongoData = "";
-		
-		cursorBear.hasNext();
-		
-		Map<Integer, String> bearData = new HashMap<Integer, String>();
-	    i = 0;
-	    while (cursorBear.hasNext()) 
-	    {
-	    	mongoData = cursorBear.next().toString();
-	    	JSONObject json = (JSONObject)new JSONParser().parse(mongoData);		
-			body = (String) json.get("body");
-				
-			theCols = body.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?");
-				
-			theCols[0] = theCols[0].toLowerCase();
-			theCols[0] = theCols[0].replaceAll("\\P{L}", " ");
-			theCols[0] = theCols[0].replaceAll(" +", " ");
-				
-			String[] theOut = theCols[0].split(" ");
-			List<String> myList = new ArrayList<String>();
-			for(int k = 0; k < theOut.length; k++)
-			{
-				if(!(theOut[k] == null))
-				{
-					myList.add(theOut[k].replaceAll("[^\\w\\s]",""));
-				}
-			}
-			theOut = myList.toArray(new String[0]);
-			String temp = "";
-			for(int k = 0; k < theOut.length; k++)
-			{
-				if (k == 0)
-				{
-					temp =  theOut[k];
-				}
-				if (k > 0)
-				{
-					temp = temp + " " + theOut[k]; 
-				}
-			} 
-			if(temp != "")
-			{
-				bearData.put(i,temp);
-				i++;
-			}
-	    }
-	    cursorBear.close();
-	    
-	   // bearData.put(i,"apple fails to beat analyst estimates");
-		
-		///BULL DATA///
-		DBObject getSentimentBull = new BasicDBObject();
-		List<BasicDBObject> objBull = new ArrayList<BasicDBObject>();
-		objBull.add(new BasicDBObject("sentiment", "Bullish"));
-		getSentimentBull.put("$or", objBull);	
-		
-		Mongo mBull = new Mongo(amznServer , Integer.parseInt(port));
-		DB dbBull = mBull.getDB(theDb);
-		DBCollection collBull = dbBull.getCollection(theCol);
-		
-		DBCursor cursorBull = collBull.find(getSentimentBull).sort(new BasicDBObject("theDate",1)).limit(neutData.size()*2);
-		//System.out.println(2*bearData.size());
-		
-		Map<Integer, String> bullData = new HashMap<Integer, String>();
-	    i = 0;
-	    while (cursorBull.hasNext()) 
-	    {
-	    		mongoData = cursorBull.next().toString();
-	    		JSONObject json = (JSONObject)new JSONParser().parse(mongoData);		
-				body = (String) json.get("body"); 
-				
-				theCols = body.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?");
-				
-				theCols[0] = theCols[0].toLowerCase();
-				theCols[0] = theCols[0].replaceAll("\\P{L}", " ");
-				theCols[0] = theCols[0].replaceAll(" +", " ");
-				
-				String[] theOut = theCols[0].split(" ");
-				
-				List<String> myList = new ArrayList<String>();
-				for(int k = 0; k < theOut.length; k++)
-				{	
-					if(!(theOut[k] == null))
-					{
-						myList.add(theOut[k].replaceAll("[^\\w\\s]",""));
-					}
-				}
-				
-				theOut = myList.toArray(new String[0]);
-				
-				if(theOut.length > 0)
-				{
-					String temp = "";
-					for(int k = 0; k < theOut.length; k++)
-					{	
-						if (k == 0)
-						{
-							temp =  theOut[k];
-						}
-						if (k > 0)
-						{
-							temp = temp + " " + theOut[k]; 
-						}
-					}
-					if(temp != "")
-					{
-						bullData.put(i,temp);
-						i++;
-					}
-				}
-	    }
-	    cursorBull.close();
-	    // System.out.println(cursorBear.size());
-	    
-	    //int theMin = Math.min(bearData.size(),bullData.size());
-	    //theMin = Math.min(theMin,neutData.size());
-	    
-	  //  int theTrain = (int) (2*theMin*theSlice);
-	  //  int theTest = 2*theMin - theTrain;
-		
-	    int theTrain = (int) (3*(neutData.size())*theSlice);
-		int theTest = (int) (3*(neutData.size()) - theTrain);
+
+		int theMin = neutData.size();
+	    int theTrain = (int) (theMin*theSlice);
 	    
 	    //TRAIN
 	    writerTrain.append("@relation 'test'");
@@ -303,32 +223,40 @@ public static void main(String[] args) throws Exception
     	
     	writerTrain.append("@data");
     	writerTrain.append('\n');
-    	
+
     	int neutSizeTrain = (int) (neutData.size()*theSlice);
     	int neutSizeTest = (int) (neutData.size()*(1-theSlice));
     	System.out.println(neutSizeTrain);
     	System.out.println(neutSizeTest);
-	    for(i=0;i<neutSizeTrain;i++)
+
+    	for(int i=0;i<neutSizeTrain;i++)
 	    {
-			writerTrain.append("\""+bearData.get(i)+"\"" + "," + -1);	
-			writerTrain.append('\n');
 	    	writerTrain.append("\""+neutData.get(i)+"\"" + "," + 0);	
-			writerTrain.append('\n');	
-	    	writerTrain.append("\""+bullData.get(i)+"\"" + "," + 1);
 			writerTrain.append('\n');
-			//System.out.println(neutData.get(i));
 	    }
-	    
-	    for(i=0;i<(cursorBear.size() - neutSizeTrain)/2;i++)
+
+    	int bullSizeTrain = (int) (bullData.size()*theSlice);
+    	int bullSizeTest = (int) (bullData.size()*(1-theSlice));
+    	System.out.println(bullSizeTrain);
+    	System.out.println(bullSizeTest);
+
+    	for(int i=0;i<bullSizeTrain;i++)
 	    {
-	    	//writerTrain.append("\""+neutData.get(i)+"\"" + "," + 0);	
-			//writerTrain.append('\n');
 	    	writerTrain.append("\""+bullData.get(i)+"\"" + "," + 1);	
 			writerTrain.append('\n');
-			writerTrain.append("\""+bearData.get(i)+"\"" + "," + -1);	
+	    }
+
+    	int bearSizeTrain = (int) (bearData.size()*theSlice);
+    	int bearSizeTest = (int) (bearData.size()*(1-theSlice));
+    	System.out.println(bearSizeTrain);
+    	System.out.println(bearSizeTest);
+
+    	for(int i=0;i<neutSizeTrain;i++)
+	    {
+	    	writerTrain.append("\""+bearData.get(i)+"\"" + "," + -1);	
 			writerTrain.append('\n');
 	    }
-	    
+  	
 		writerTrain.flush();
 		writerTrain.close();
 		
@@ -347,31 +275,26 @@ public static void main(String[] args) throws Exception
 		writerTest.append("@data");
 		writerTest.append('\n');
 
-		for(i=0;i<neutSizeTest ;i++)
+		for(int i=0;i<neutSizeTest ;i++)
 		{
-		    writerTest.append("\""+bearData.get(i+neutSizeTrain)+"\"" + "," + -1);	
-		    writerTest.append('\n');
 			writerTest.append("\""+neutData.get(i+neutSizeTrain)+"\"" + "," + 0);	
 		    writerTest.append('\n');
-			writerTest.append("\""+bullData.get(i+neutSizeTrain)+"\"" + "," + 1);
-		    writerTest.append('\n');
-		   // System.out.println(neutSizeTrain);
-		   // System.out.println(neutData.size());
 		}
-		
-		for(i=0;i<(cursorBear.size()-neutSizeTest)/2;i++)
+
+		for(int i=0;i<bullSizeTest ;i++)
 		{
-			writerTest.append("\""+bullData.get(i+theTrain/2)+"\"" + "," + 1);	
-		    writerTest.append('\n');
-		    writerTest.append("\""+bearData.get(i+theTrain/2)+"\"" + "," + -1);	
+			writerTest.append("\""+bullData.get(i+bullSizeTrain)+"\"" + "," + 1);	
 		    writerTest.append('\n');
 		}
-		
+
+		for(int i=0;i<bearSizeTest ;i++)
+		{
+			writerTest.append("\""+bearData.get(i+bearSizeTrain)+"\"" + "," + -1);	
+		    writerTest.append('\n');
+		}
+
 		writerTest.flush();
 		writerTest.close();
-	
-		bullData = null;
-		bearData = null;
 		
 		////////////////////////////
 		///END PREPROCSSING STAGE///
@@ -394,12 +317,8 @@ public static void main(String[] args) throws Exception
 		optionsClassifier[13] = "1";
 		optionsClassifier[14] = "-tokenizer";	
 		optionsClassifier[15] = "weka.core.tokenizers.NGramTokenizer";
-	//	optionsClassifier[16] = "-min";
-	//	optionsClassifier[17] = "5";
-	//	optionsClassifier[18] = "-max";
-	//	optionsClassifier[19] = "5";
 		
-		BufferedReader reader = new BufferedReader(new FileReader("myPATH/train.arff"));
+		BufferedReader reader = new BufferedReader(new FileReader("/home/ubuntu/code/theData/train.arff"));
 		Instances train = new Instances(reader);
 		reader.close();
 		train.setClassIndex(train.numAttributes()-1);
@@ -425,7 +344,7 @@ public static void main(String[] args) throws Exception
 		
 		classifier.buildClassifier(train);
 		
-		BufferedReader readerTest = new BufferedReader(new FileReader("myPATH/test.arff"));
+		BufferedReader readerTest = new BufferedReader(new FileReader("/home/ubuntu/code/theData/test.arff"));
 		Instances test = new Instances(readerTest);
 		readerTest.close();
 		test.setClassIndex(test.numAttributes()-1);
@@ -441,7 +360,7 @@ public static void main(String[] args) throws Exception
 		{
 			System.out.println("SAVING MODEL, ACCURACY IS: " + pctCorr);
 			ObjectOutputStream myModelWrite = new ObjectOutputStream(
-	        new FileOutputStream("myPATH/" + theModel + ".model"));
+	        new FileOutputStream("/home/ubuntu/code/theData/" + theModel + ".model"));
 			myModelWrite.writeObject(classifier);
 			myModelWrite.flush();
 			myModelWrite.close();
